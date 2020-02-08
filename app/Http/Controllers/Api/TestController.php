b862434298135fe76489499f08cf6302974a8a2a
haiou <haiou@localhost.localdomain>
2020-02-08 11:57:27 +0800

    112

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -209,7 +209,7 @@ public function sign()
             ]
         ]);
 
-        //接收服务器端响应的数据
+        //接收服务器端响应的数据1
         $response_data = $response->getBody();
         echo $response_data;
 
